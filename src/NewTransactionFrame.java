42a984b08d0298e4b04c1f3242cccba60c2b82c3
StellarProcy <procyu@gmail.com>
2015-07-06 13:45:37 +0200

    Заготовка для окна транзакций

---
+++ src/NewTransactionFrame.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NewTransactionFrame extends JFrame {
+  private JButton addTransaction;
+  private JButton removeTransaction;
+public static void main(String[] args) {
+    NewTransactionFrame app = new NewTransactionFrame();
+    app.setVisible(true);
+    app.pack(); //Оптимальный размер в зависимости от содержимого окна
+  }
+}
